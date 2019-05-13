8d0ee57442eacc82452d95c223d8cc30372a170a
James Kirwa <jameskirwa34@gmail.com>
2019-05-13 10:19:13 +0300

    change form to paragraph

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+var totalPlayer1 = 0;
+var totalPlayer2 = 0;
 $(document).ready(function(){
   $("#button").click(function(){
     $(".names").show();
 
   });
+  $("#diceP1").click(function(){
+    james();
+  })
+  var james = function (){
+    var sum = Math.floor((Math.random()*6)+1);
+    if (sum===1) {
+        alert("player1 rolled 1");
+    }else{
+      totalPlayer1 += sum;
+      // totalPlayer1 = totalPlayer1 + sum
+    }
+    $("#player1Score").text(sum);
+    $("#player1Total").text(totalPlayer1);
+  });
+  
+  $("#diceP2").click(function() {
+    kirwa();
+  })
+  var kirwa = function (){
+    var sum =Math.floor((Math.random()*6)+1);
+    if (sum===1) {
+      alert(player2 rolled);
+    } else {
+    totalPlayer2 += sum;
+    }
+    $("#player2Score").text(sum);
+    $("#player2Total").text(totalPlayer2);
+  }
   // function player(name,roll,total,active,playing) {
   //   this.name = name;
   //   this.roll =0;
